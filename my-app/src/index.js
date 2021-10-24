2cb090665946380a26b20e14bfd3319bbf13e7cc
K2G90 <clw.jr90@gmail.com>
2021-10-23 21:02:36 -0400

    updated index.js file with NavLink

--- my-app/src/index.js
+++ my-app/src/index.js
@@ -3,13 +3,21 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import Users from './components/Users';
-import { BrowserRouter, Route } from 'react-router-dom';
-import { Profile } from './components/Profile'
+import { BrowserRouter, Route, NavLink } from 'react-router-dom';
+import Profile from './components/Profile'
 
 const Root = () => {
+  const handleClick = () =>{
+    console.log('Thanks for clicking!')
+  };
   return (
     <BrowserRouter>
       <div>
+        <NavLink to="/">App</NavLink>
+        <NavLink activeClassName="red" to="/users">Users</NavLink>
+        <NavLink activeClassName="blue" to="/hello">Hello</NavLink>
+        <NavLink activeClassName="green" to="/users/1">Kendrick's Profile</NavLink>
+        <NavLink to="/" onClick={handleClick}>App with click handler</NavLink>
         <Route exact path="/">
           <App />
         </Route>
